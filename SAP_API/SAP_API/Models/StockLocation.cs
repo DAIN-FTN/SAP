7c484ab23992143ef93b5ea77342214239afacb4
NatalijaGajic <nat.gaj98@gmail.com>
2023-01-28 12:55:00 +0100

    add SAP API setup

---
+++ SAP_API/SAP_API/Models/StockLocation.cs
@@ -0,0 +1,10 @@
+﻿namespace SAP_API.Models
+{
+    public class StockLocation
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public int Capacity { get; set; }
+
+    }
+}
---
+++ SAP_API/SAP_API/Models/StockedProduct.cs
@@ -0,0 +1,13 @@
+﻿namespace SAP_API.Models
+{
+    public class StockedProduct
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public StockLocation StockLocation {get; set; }
+        public int Quantity { get; set; }
+        public int ReservedQuantity { get; set; } 
+
+
+    }
+}
