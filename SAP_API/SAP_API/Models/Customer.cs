8c033ea3a60bd9c0e583a478aaaa17743c6713a8
NatalijaGajic <nat.gaj98@gmail.com>
2023-01-28 13:21:12 +0100

    finish models setup

---
+++ SAP_API/SAP_API/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace SAP_API.Models
+{
+    public class Customer
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+    }
+}
--- SAP_API/SAP_API/Models/OrderProduct.cs
+++ SAP_API/SAP_API/Models/OrderProduct.cs
@@ -2,5 +2,9 @@
 {
     public class OrderProduct
     {
+        public Guid Id { get; set; }
+        public Product Product { get; set; }
+        public Order Order { get; set; }
+        public int Quantity { get; set; }
     }
 }
