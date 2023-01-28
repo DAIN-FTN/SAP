df686faf24ea6581911a3f757d169b5ac2783257
NatalijaGajic <nat.gaj98@gmail.com>
2023-01-28 16:24:19 +0100

    add product service setup

--- SAP_APIv1/SAP_API/Services/ProductService.cs
+++ SAP_APIv1/SAP_API/Services/ProductService.cs
@@ -1,4 +1,6 @@
 ﻿using SAP_API.DTOs;
+using SAP_API.Mappers;
+using SAP_API.Models;
 using SAP_API.Repositories;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,32 @@ namespace SAP_API.Services
     public class ProductService : IProductService
     {
         private readonly IProductRepository _productRepository;
+        private readonly IStockedProductRepository _stockedProductRepository;
+
+        public ProductService(IProductRepository productRepository, IStockedProductRepository stockedProductRepository)
+        {
+            _productRepository = productRepository;
+            _stockedProductRepository = stockedProductRepository;
+
+        }
 
         public List<ProductStockDTO> GetProductStock(string name)
         {
-            throw new NotImplementedException();
+            List<ProductStockDTO> resultList = new List<ProductStockDTO>();
+            List<Product> products = _productRepository.GetByName(name);
+            foreach(Product prod in products)
+            {
+                List<StockedProduct> stockedProducts = _stockedProductRepository.GetByProductId(prod.Id);
+                int availableQuantity = 0;
+                foreach(StockedProduct stockedProd in stockedProducts)
+                {
+                    availableQuantity += stockedProd.Quantity - stockedProd.ReservedQuantity;
+                }
+                ProductStockDTO dto = ProductMapper.CreateProductStockDTO(prod, availableQuantity);
+                resultList.Add(dto);
+            }
+
+            return resultList;
         }
     }
 }
