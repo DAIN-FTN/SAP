f3c4cb6b44d55d14504e933acff9c3095c34448d
NatalijaGajic <nat.gaj98@gmail.com>
2023-01-28 16:12:40 +0100

    add stockedproduct repository setup

---
+++ SAP_APIv1/SAP_API/Repositories/StockedProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using SAP_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAP_API.Repositories
+{
+    public class StockedProductRepository : IStockedProductRepository
+    {
+        public StockedProduct Create(StockedProduct entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<StockedProduct> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StockedProduct GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StockedProduct> GetByProductId(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StockedProduct Update(StockedProduct entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
