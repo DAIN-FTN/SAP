7e7bd056ed575049d4a363a8ea5d7eeefb4f3047
L-48\Vladimir <vladimir.filipovic@tiacgroup.com>
2023-01-29 15:33:29 +0100

    orchestrate order creation method fix

--- SAP_APIv1/SAP_API/Services/OrderService.cs
+++ SAP_APIv1/SAP_API/Services/OrderService.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using SAP_API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SAP_API.Services
 {
-    public class OrderService
+    public class OrderService : IOrderService
     {
-
+        public bool CreateOrder(DateTime shouldBeDoneAt, Customer customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
