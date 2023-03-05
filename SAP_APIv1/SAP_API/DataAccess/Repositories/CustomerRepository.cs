610056e6f92f52763d60de3302775d2f21bc8e3c
L-48\Vladimir <vladimir.filipovic@tiacgroup.com>
2023-03-05 20:17:42 +0100

    Added Customer repository

---
+++ SAP_APIv1/SAP_API/DataAccess/Repositories/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using SAP_API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SAP_API.DataAccess.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Customer Create(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
