aa93ff3c22a0254d9f6a80ecdc2c4accf0b09dc0
NatalijaGajic <nat.gaj98@gmail.com>
2023-04-23 15:22:59 +0200

    DbContext: add migrations to fix bakingprogram entity

--- SAP_APIv1/SAP_API/Models/BakingProgram.cs
+++ SAP_APIv1/SAP_API/Models/BakingProgram.cs
@@ -17,8 +17,10 @@ namespace SAP_API.Models
         public DateTime BakingProgrammedAt { get; set; }
         public DateTime? BakingStartedAt { get; set; }
         public DateTime? BakingEndsAt { get; set; }
+        public Guid OvenId { get; set; }
+
         public Oven Oven { get; set; }
-        public Guid PreparedByUserId { get; set; }
+        public Guid? PreparedByUserId { get; set; }
         public User PreparedByUser { get; set; }
         public int RemainingOvenCapacity { get; set; }
         public List<BakingProgramProduct> Products { get; set; }
