847ae13808400b5f9e0e0fcb920cc3cec2550f0b
NatalijaGajic <nat.gaj98@gmail.com>
2023-04-25 14:34:57 +0200

    Auth: resolve build errors, remove unused imports

--- SAP_APIv1/SAP_API/Controllers/IdentityController.cs
+++ SAP_APIv1/SAP_API/Controllers/IdentityController.cs
@@ -1,13 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SAP_API.DTOs.Requests;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
-using Microsoft.IdentityModel.JsonWebTokens;
-using System;
-using System.Security.Claims;
+
 using SAP_API.Services;
 using SAP_API.Models;
 
